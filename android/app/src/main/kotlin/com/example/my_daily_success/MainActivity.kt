7d80139186c559e4d2250de92bfc2d47c1d26bd9
Michael Debertol <michael.debertol@gmail.com>
2020-06-16 18:31:55 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/my_daily_success/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_daily_success
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.red,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.red,
+            ),
+            title: Text("Startseite"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.show_chart,
+              color: Colors.grey,
+            ),
+            title: Text("Level"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.grade,
+              color: Colors.grey,
+            ),
+            title: Text("Mutmacher"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: Colors.grey,
+            ),
+            title: Text("Profil"),
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: Colors.red,
+        onTap: (_) {},
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        tooltip: "Hinzufügen",
+      ),
+    );
+  }
+}
