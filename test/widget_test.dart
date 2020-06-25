a46b7f8f448c19fb3540cf28b554acb61ef1d35d
Michael Debertol <michael.debertol@gmail.com>
2020-06-25 12:06:06 +0200

    remove unused import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:my_daily_success/level.dart';
 
-import 'package:my_daily_success/main.dart';
 import 'package:tuple/tuple.dart';
 
 void main() {
