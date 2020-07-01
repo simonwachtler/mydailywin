1caec7a853b42485303fc97736af98b9099a68ed
Gernot Wachtler <gernotwachtler@macbook\032air\032von\032gernot.homenet.telecomitalia.it>
2020-07-01 16:49:20 +0200

    show tken pic

--- lib/new_camera_entry.dart
+++ lib/new_camera_entry.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
@@ -81,6 +83,14 @@ class _NewCameraEntryState extends State<NewCameraEntry> {
 
                   // Attempt to take a picture and log where it's been saved.
                   await _controller.takePicture(path);
+                  // If the picture was taken, display it on a new screen.
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          DisplayPictureScreen(imagePath: path),
+                    ),
+                  );
                 } catch (e) {
                   // If an error occurs, log the error to the console.
                   print(e);
@@ -93,6 +103,23 @@ class _NewCameraEntryState extends State<NewCameraEntry> {
           return Center(child: CircularProgressIndicator());
         }
       },
+    );
+  }
+}
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
     );
   }
 }
