82fba670e57d0a3703be2eae4d745bdc2ab932f5
Michael Debertol <michael.debertol@gmail.com>
2020-07-01 16:52:31 +0200

    boxfit

--- lib/new_camera_entry.dart
+++ lib/new_camera_entry.dart
@@ -119,7 +119,11 @@ class DisplayPictureScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Display the Picture')),
       // The image is stored as a file on the device. Use the `Image.file`
       // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
+      body: Container(
+        child: Image.file(File(imagePath), fit: BoxFit.cover),
+        height: double.infinity,
+        width: double.infinity,
+      ),
     );
   }
 }
