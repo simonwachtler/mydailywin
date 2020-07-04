71acfbe3583451c77e65c6e1f48e7e6735cbd705
Michael Debertol <michael.debertol@gmail.com>
2020-07-04 23:03:57 +0200

    don't allow going back in onboarding

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'animations.dart';
 import 'main.dart';
@@ -14,58 +15,65 @@ class _FirstImpressionState extends State<FirstImpression> {
   final _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: AnimatedListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 118.0),
-            child: Image.asset(
-              'assets/win-grey.png',
-              height: 220,
-              width: 250,
+    return WillPopScope(
+      onWillPop: () async {
+        SystemNavigator.pop();
+        return false;
+      },
+      child: Scaffold(
+        body: AnimatedListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 118.0),
+              child: Image.asset(
+                'assets/win-grey.png',
+                height: 220,
+                width: 250,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 1),
-            child: Text(
-              "Herzlich Willkommen \nbei My Daily Win!",
-              style: Theme.of(context).textTheme.headline4,
-              textAlign: TextAlign.center,
+            Padding(
+              padding: const EdgeInsets.only(bottom: 1),
+              child: Text(
+                "Herzlich Willkommen \nbei My Daily Win!",
+                style: Theme.of(context).textTheme.headline4,
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 30),
-            child: Center(
-                child: Text("Bevor wir gleich loslegen: Dein Vorname, bitte!")),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              style: TextStyle(fontSize: 18),
-              textAlign: TextAlign.center,
-              controller: _controller,
-              onChanged: (n) => setState(() {}),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Center(
+                  child:
+                      Text("Bevor wir gleich loslegen: Dein Vorname, bitte!")),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: RaisedButton(
-              elevation: 7,
-              color: Colors.blue,
-              onPressed: _controller.text.isNotEmpty
-                  ? () async {
-                      setData(() => data.name = _controller.text);
-                      await Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Onboarding()),
-                      );
-                      Navigator.pop(context);
-                    }
-                  : null,
-              child: Text("Jetzt loslegen!"),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextField(
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+                controller: _controller,
+                onChanged: (n) => setState(() {}),
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: RaisedButton(
+                elevation: 7,
+                color: Colors.blue,
+                onPressed: _controller.text.isNotEmpty
+                    ? () async {
+                        setData(() => data.name = _controller.text);
+                        await Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Onboarding()),
+                        );
+                        Navigator.pop(context);
+                      }
+                    : null,
+                child: Text("Jetzt loslegen!"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -79,54 +87,61 @@ class Onboarding extends StatefulWidget {
 class _OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: AnimatedColumn(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 118.0),
-            child: Image.asset(
-              'assets/rakete.png',
-              height: 220,
-              width: 250,
+    return WillPopScope(
+      onWillPop: () async {
+        SystemNavigator.pop();
+        return false;
+      },
+      child: Scaffold(
+        body: AnimatedColumn(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 118.0),
+              child: Image.asset(
+                'assets/rakete.png',
+                height: 220,
+                width: 250,
+              ),
             ),
-          ),
-          Text(
-            "So funktioniert's:",
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Text(
-              "Notiere jeden Tag 5 Erfolge, \nbaue dadurch Dein Selbsvertrauen auf \nund gewinne Mut und Motivation \nfür neue Herausforderungen ",
-              textAlign: TextAlign.center,
+            Text(
+              "So funktioniert's:",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             ),
-          ),
-          SwitchListTile.adaptive(
-            title: Text("Am Morgen erinnern"),
-            subtitle: Text("Ich möchte morgens ans Eintragen erinnert werden"),
-            value: data.dailyNotificationsEnabled,
-            onChanged: (enabled) {
-              setState(() {
-                setData(() {
-                  data.dailyNotificationsEnabled = enabled;
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                "Notiere jeden Tag 5 Erfolge, \nbaue dadurch Dein Selbsvertrauen auf \nund gewinne Mut und Motivation \nfür neue Herausforderungen ",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SwitchListTile.adaptive(
+              title: Text("Am Morgen erinnern"),
+              subtitle:
+                  Text("Ich möchte morgens ans Eintragen erinnert werden"),
+              value: data.dailyNotificationsEnabled,
+              onChanged: (enabled) {
+                setState(() {
+                  setData(() {
+                    data.dailyNotificationsEnabled = enabled;
+                  });
                 });
-              });
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: RaisedButton(
-              elevation: 7,
-              color: Colors.blue,
-              onPressed: () async {
-                Navigator.pop(context);
-                await initializeNotifications(context);
-                updateNotifications();
               },
-              child: Text("Los geht's!"),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                elevation: 7,
+                color: Colors.blue,
+                onPressed: () async {
+                  Navigator.pop(context);
+                  await initializeNotifications(context);
+                  updateNotifications();
+                },
+                child: Text("Los geht's!"),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
