4fd48763122b91be4b20c4460743992041927924
Michael Debertol <michael.debertol@gmail.com>
2020-07-14 15:28:11 +0200

    typo

--- lib/greeting.dart
+++ lib/greeting.dart
@@ -67,7 +67,7 @@ class _GreetingState extends State<Greeting> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text(
-                "NEUSES LEVEL ERREICHEN:",
+                "NEUES LEVEL ERREICHEN:",
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               Text("Jetzt neuen Erfolg notieren"),
